3b26675ce8e5461cb4cd6278aa879bbbab307b22
Maillait <williamottenbreit@gmail.com>
2025-09-15 20:52:27 -0600

    Update WebDesign.js

--- WebDesign.js
+++ WebDesign.js
@@ -1,19 +1,24 @@
+// keeps track of mouse position
 document.addEventListener("mousemove", (event) => {
   mouseY = event.clientY;
   mouseX = event.clientX;
 });
 
+// declaring variable that store follower and cursor position
 let mouseX = 0;
 let mouseY = 0;
 let followerX = 0;
 let followerY = 0;
 
 let interval = setInterval(function () {
+  // updating the follower position
   followerX += (mouseX - followerX) / 10;
   followerY += (mouseY - followerY) / 10;
 
+  // re-drawing the follower
   document.getElementById("follower").style.bottom =
     window.innerHeight - followerY - 100 + "px";
   document.getElementById("follower").style.right =
     window.innerWidth - followerX - 100 + "px";
 }, 50);
+
