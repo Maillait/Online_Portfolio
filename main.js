6de28b404d407387f87d02688682b0c63b01714c
Maillait <williamottenbreit@gmail.com>
2025-09-16 16:19:59 -0600

    Update main.js
    attempting to make the player/follower position an object

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 export let mouseX = 0;
 export let mouseY = 0;
 export let fSpeed = 20;
+let playerPos = {
+  x : 0,
+  y : 0
+}
+let interval = setInterval(mainTick(),50);
+
+function mainTick() {
+playerPos = follow(playerPos.x, playerPos.y);
+}
 
 document.addEventListener("mousemove", (event) => {
   mouseY = event.clientY;
   mouseX = event.clientX;
+
 });
