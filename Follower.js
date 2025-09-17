f25922cd6dd4e30bb70fa28e6d0ee7b45c9ce30b
Maillait <williamottenbreit@gmail.com>
2025-09-16 20:11:05 -0600

    Update Follower.js

--- Follower.js
+++ Follower.js
@@ -1,21 +1,25 @@
 import { mouseX, mouseY, fSpeed, followerX, followerY } from "./main.js";
 
+export let setFollowX;
+export let setFollowY;
+
 export function follow() {
-  followerX += (mouseX - followerX) / fSpeed;
-  followerY += (mouseY - followerY) / fSpeed;
+  setFollowX += (mouseX - followerX) / fSpeed;
+  setFollowY += (mouseY - followerY) / fSpeed;
 
   document.getElementById("follower").style.bottom =
-    window.innerHeight - followerY - 100 + "px";
+    window.innerHeight - setFollowY - 100 + "px";
   document.getElementById("follower").style.right =
-    window.innerWidth - followerX - 100 + "px";
+    window.innerWidth - setFollowX - 100 + "px";
 
-  console.log(followerX, followerY);
+  console.log(setFollowX, setFollowY);
   return;
 }
 
 export function customize(image) {
   
 }
+
 
 
 
