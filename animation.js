7be54236c6c086382ea21e7eb88edebc7036b4dd
Maillait <williamottenbreit@gmail.com>
2025-09-19 20:59:25 -0600

    Update animation.js

--- animation.js
+++ animation.js
@@ -9,7 +9,6 @@ export function animateFollower() {
   // if distance moved is less than 5 pixels
   if (Math.sqrt((((mouseX - followerX) / fSpeed) * ((mouseX - followerX) / fSpeed)) + (((mouseY - followerY) / fSpeed) * ((mouseY - followerY) / fSpeed))) < 5) {
     document.getElementById("charSprite").style.left = 0;
-    console.log(Math.sqrt((((mouseX - followerX) / fSpeed) * ((mouseX - followerX) / fSpeed)) + (((mouseY - followerY) / fSpeed) * ((mouseY - followerY) / fSpeed))));
   } else {
     
     if (frame > 5) {
