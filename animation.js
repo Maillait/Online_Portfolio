dbf1777b8401e982504abfce62a04d5d9afee738
Maillait <williamottenbreit@gmail.com>
2025-09-20 09:57:17 -0600

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,19 +1,19 @@
 import { mouseX, mouseY, fSpeed, followerX, followerY } from "./main.js";
 
 let walkToggle = 0;
-let frame = 0;
+let distanceSum = 0;
 
 export function animateFollower() {
-  frame++;
+  distanceSum += Math.sqrt((((mouseX - followerX) / fSpeed) * ((mouseX - followerX) / fSpeed)) + (((mouseY - followerY) / fSpeed) * ((mouseY - followerY) / fSpeed)));
   
   // if distance moved is less than 5 pixels
   if (Math.sqrt((((mouseX - followerX) / fSpeed) * ((mouseX - followerX) / fSpeed)) + (((mouseY - followerY) / fSpeed) * ((mouseY - followerY) / fSpeed))) < 5) {
     document.getElementById("charSprite").style.left = "0px";
   } else {
     
-    if (frame > 5) {
+    if (distanceSum > 5) {
       walkToggle = !walkToggle;
-      frame = 0;
+      distanceSum = 0;
     }
     
     if (walkToggle) document.getElementById("charSprite").style.left = "-79px";
